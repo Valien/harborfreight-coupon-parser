e600cfda299bdc81a6d56985f4a950eac4ee50c3
Allen Vailliencourt <chumpmonkey@gmail.com>
2019-05-20 14:22:00 -0400

    Updated query and pipfile.

--- queryhf.py
+++ queryhf.py
@@ -3,8 +3,9 @@
 import requests
 
 # https://images.harborfreight.com/hftweb/campaigns/localmedia/digitalsavings/Nov2018/digitalsavings_01.png'
+# as of this update (may 2019) the format has changed: https://images.harborfreight.com/hftweb/campaigns/localmedia/digitalsavings/2019/06_jun/digitalsavings_01.png
 
-BaseURL = 'https://images.harborfreight.com/hftweb/campaigns/localmedia/digitalsavings/'
+BaseURL = 'https://images.harborfreight.com/hftweb/campaigns/localmedia/digitalsavings/2019/'
 
 
 def get_images(moyr, numimages):
@@ -35,7 +36,7 @@ def get_images(moyr, numimages):
             else:
                 print(f'Error! {response.status_code}')
 
-my_input = input('Enter Month and Year in this format - Nov2018, Dec2018, etc.: ')
+my_input = input('Enter Month and Year in this format - 05_may, 06_jun, etc.: ')
 numrange_input = int(input('Enter range of numbers between 1-30: '))
 print(f'You entered {my_input} for the month/year and a range of {numrange_input} for the amount of images to be downloaded.')
 #print(f'Now parsing images from {BaseURL}\n')
