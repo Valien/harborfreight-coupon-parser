143281ff78ec65502ee7d68fef34cf6b4489db4a
Allen Vailliencourt <chumpmonkey@gmail.com>
2019-07-17 10:55:38 -0400

    HF changed formats. No barcode now.

--- queryhf.py
+++ queryhf.py
@@ -4,9 +4,10 @@ import requests
 
 # https://images.harborfreight.com/hftweb/campaigns/localmedia/digitalsavings/Nov2018/digitalsavings_01.png'
 # as of this update (may 2019) the format has changed: https://images.harborfreight.com/hftweb/campaigns/localmedia/digitalsavings/2019/06_jun/digitalsavings_01.png
+# Another update - July - https://images.harborfreight.com/hftweb/weblanding/home-coupons-july2019/images/02.jpg
 
-BaseURL = 'https://images.harborfreight.com/hftweb/campaigns/localmedia/digitalsavings/2019/'
-
+#BaseURL = 'https://images.harborfreight.com/hftweb/campaigns/localmedia/digitalsavings/2019/'
+BaseURL = 'https://images.harborfreight.com/hftweb/weblanding/home-coupons-'
 
 def get_images(moyr, numimages):
     print('Acquiring images...')
@@ -16,27 +17,27 @@ def get_images(moyr, numimages):
             This function get's the images. Yar! Does require you to enter Month and Number of images.
         '''    
         if n < 10:
-            urltoparse = BaseURL + moyr + '/digitalsavings_' + str(n).zfill(2) + ".png"
+            urltoparse = BaseURL + moyr + '/images/' + str(n).zfill(2) + ".jpg"
             print(urltoparse)
             response = requests.get(urltoparse)  # + str(n))
             if response.status_code == 200:
                 # to do: remove my hard-coded desktop location...
-                with open("/Users/allen.vailliencourt/Desktop/hf/" + moyr + '-' + str(n) + '.png', 'wb') as f:
+                with open("/Users/allen.vailliencourt/Desktop/hf/" + moyr + '-' + str(n) + '.jpg', 'wb') as f:
                     f.write(response.content)
             else:
                 print(f'Error! {response.status_code}')
         else:
-            urltoparse = BaseURL + moyr + '/digitalsavings_' + str(n) + ".png"
+            urltoparse = BaseURL + moyr + '/images/' + str(n) + ".jpg"
             print(urltoparse)
             response = requests.get(urltoparse)
             if response.status_code == 200:
                 # to do: see above.
-                with open("/Users/allen.vailliencourt/Desktop/hf/" + moyr + '-' + str(n) + '.png', 'wb') as f:
+                with open("/Users/allen.vailliencourt/Desktop/hf/" + moyr + '-' + str(n) + '.jpg', 'wb') as f:
                     f.write(response.content)
             else:
                 print(f'Error! {response.status_code}')
 
-my_input = input('Enter Month and Year in this format - 05_may, 06_jun, etc.: ')
+my_input = input('Enter Month and Year in this format - july2019, etc.: ')
 numrange_input = int(input('Enter range of numbers between 1-30: '))
 print(f'You entered {my_input} for the month/year and a range of {numrange_input} for the amount of images to be downloaded.')
 #print(f'Now parsing images from {BaseURL}\n')
